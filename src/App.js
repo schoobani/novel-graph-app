6c1c77a42c32fb54c7dc4b4e23f3fa013ecb9c60
schoobani <saeed.choobani@gmail.com>
2023-04-05 16:36:27 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route} from "react-router-dom";
-import Home from "./pages/Home.js"
 import Dashboard from "./pages/dashboard.js"
 
 export default function App() {
     return (
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </div>
     );
