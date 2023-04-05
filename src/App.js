696dceceefa962ac9dace7d697b869684f251cdb
schoobani <saeed.choobani@gmail.com>
2023-04-05 17:10:49 +0200

    remove routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { Routes, Route} from "react-router-dom";
 import Dashboard from "./pages/dashboard.js"
 
 export default function App() {
     return (
       <div className="App">
-        <Routes>
-          <Route path="/{novel-graph-app}" element={<Dashboard />} />
-        </Routes>
+       <Dashboard />
       </div>
     );
   }
